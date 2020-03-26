f2fbac288376fd7c2286ac8b0d1e5368b0602911
zamoroz <moras8465132@gmail.com>
2020-03-26 20:58:43 +0700

    4)	Добавьте кнопку Gen, которая будет выводить новую фразу

--- app/src/main/java/com/example/lr3/MainActivity.java
+++ app/src/main/java/com/example/lr3/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.lr3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button gen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        gen = findViewById(R.id.button);
 
-        textView.setText("Всё, что нам нужно – это " + Phraser.PhraserGen());
+        textView.setText(generate());
+
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(generate());
+            }
+        });
+
+    }
+
+    String generate(){
+        return  "Всё, что нам нужно – это " + Phraser.PhraserGen();
     }
 }
